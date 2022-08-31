39f3925c3dbabed976dc806b1e59c32db4bf83dc
Parshukov Alexandr <parshukovae@yandex.ru>
2022-08-31 20:34:30 +0300

    404

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Main from '../Main/Main';
+import NotFound from '../NotFound/NotFound';
 
 function App() {
   return (
     <div className="page">
+      <NotFound />
       <Main />
     </div>
   );
