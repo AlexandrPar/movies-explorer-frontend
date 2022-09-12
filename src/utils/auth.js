25109376bd06472b3189ed07ece7977fb6c178e1
Parshukov Alexandr <parshukovae@yandex.ru>
2022-09-12 20:05:12 +0300

    создание новой ветки

---
+++ src/utils/auth.js
@@ -0,0 +1,38 @@
+const BASE_URL = 'https://api.parshukov.movies.nomoredomains.sbs';
+
+function getRequest(res) {
+  if (res.ok) {
+    return res.json()
+  }
+  return Promise.reject(`Ошибка: ${res.status}`)
+}
+
+export const signup = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(getRequest);
+};
+
+export const signin = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: { 'Content-Type': "application/json" },
+    body: JSON.stringify({ email, password })
+  })
+    .then((res) => getRequest(res))
+};
+
+export const checkToken = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  }).then(getRequest);
+};
